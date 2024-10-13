2497032caea56b1253355434e381dfbc038f6c0c
Tushar Anekar <anekar.tushar@gmail.com>
2024-10-13 15:16:16 +0530

    feat - add error and loding

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -6,15 +6,13 @@ import {
 } from "../selectors/songSelector";
 
 export const SongList = () => {
-  //   const {
-  //     loading = false,
-  //     songs = [],
-  //     error = "",
-  //   } = useSelector((state) => state || {});
-
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
   const songs = useSelector(selectSongs);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+  if (songs.length === 0) return <p>No songs found. Try another search.</p>;
 
   return (
     <ul>

34fe5cd659bcf06fe122db58097e92fb04a251c3
Tushar Anekar <anekar.tushar@gmail.com>
2024-11-10 18:41:00 +0530

    chore/add-redux-devtools

--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
 import songReducer from "./reducers/songReducer";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-const store = createStore(songReducer, applyMiddleware(thunk));
+const store = createStore(
+  songReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
